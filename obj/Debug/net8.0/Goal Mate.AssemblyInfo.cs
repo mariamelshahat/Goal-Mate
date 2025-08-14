4e8a40de19a6bc92d85ccdda550668f6f7cf1e7f
mariamelshahat <mm6002@fayoum.edu.eg>
2025-08-14 15:21:03 +0300

    install Package

--- obj/Debug/net8.0/Goal Mate.AssemblyInfo.cs
+++ obj/Debug/net8.0/Goal Mate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Goal Mate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f59bbd2af9465524ccaef3bfef78dc331ade332e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Goal Mate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Goal Mate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
